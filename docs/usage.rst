deb107e490d821b3494130e4c4cccf2be2d4e06d
Morgan Bye <mbye@bcgsc.ca>
2018-09-22 15:36:45 -0700

    Project setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use finances_at_home in a project::
+
+    import finances_at_home
---
+++ finances_at_home/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for finances_at_home."""
+
+__author__ = """Morgan Bye"""
+__email__ = 'morgan@morganbye.com'
+__version__ = '0.1.0'
